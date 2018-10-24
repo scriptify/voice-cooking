e158b8846efc5f09722ccbfa81397bb6106b6b6c
Maximilian Torggler <maximilian.torggler@tuta.io>
2018-10-24 13:26:46 +0200

    Removed call to experimental API.

--- server/interfaces/graphql/index.js
+++ server/interfaces/graphql/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const util = require('util');
 const path = require('path');
 const { GraphQLServer } = require('graphql-yoga');
 
@@ -6,8 +7,10 @@ const resolvers = require('./resolvers');
 
 const recipe = require('../../bussiness/models/Recipe');
 
+const readFile = util.promisify(fs.readFile);
+
 module.exports = async function setupGraphQLServer() {
-  const typeDefs = (await fs.promises.readFile(path.resolve(__dirname, './types.graphql'))).toString();
+  const typeDefs = (await readFile(path.resolve(__dirname, './types.graphql'))).toString();
   const server = new GraphQLServer({
     typeDefs,
     resolvers,
